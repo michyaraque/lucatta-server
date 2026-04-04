3633937203919cc8dd99a819dbfff669f9aa7d54
michyaraque <michyaraque@gmail.com>
2026-04-04 18:26:45 +0200

    feat: just update ton of things

--- src/const.h
+++ src/const.h
@@ -557,7 +557,7 @@ enum item_t : uint16_t
 	ITEM_CRYSTAL_COIN = 2160,
 	ITEM_STORE_COIN = 24774, // in-game store currency
 
-	ITEM_DEPOT = 2594,
+	ITEM_DEPOT = 2002,
 	ITEM_LOCKER = 2589,
 	ITEM_INBOX = 14404,
 	ITEM_MARKET = 14405,
