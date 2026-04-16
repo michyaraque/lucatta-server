2828a63961801c3f0c0c52d2bf8e00cc00090b83
michyaraque <michyaraque@gmail.com>
2026-04-16 03:14:27 +0200

    feat: include slime monster

---
+++ data/scripts/monsters/slime.lua
@@ -0,0 +1,53 @@
+local mType = Game.createMonsterType("Slime")
+local monster = {}
+
+monster.name = "Slime"
+monster.experience = 25
+monster.outfit = {
+	lookType = 248,
+}
+monster.health = 150
+monster.maxHealth = 150
+monster.race = "blood"
+monster.speed = 150
+monster.runHealth = 5
+monster.changeTarget = {
+	interval = 4000,
+	chance = 10,
+}
+
+monster.flags = {
+	attackable = true,
+	hostile = true,
+	targetDistance = 1,
+	staticAttack = 90,
+}
+
+monster.loot = {
+	{ id = 35,  chance = 40000,  maxCount = 1 },
+	{ id = 38,  chance = 5000,   maxCount = 1 },
+	{ id = 74,  chance = 20000,  maxCount = 1 },
+	{ id = 80,  chance = 5000,   maxCount = 1 },
+	{ id = 92,  chance = 100000, maxCount = 5 },
+	{ id = 61,  chance = 100000, maxCount = 1 },
+}
+
+monster.attacks = {
+	{
+		name = "combat", type = COMBAT_PHYSICALDAMAGE, chance = 100, interval = 2000, target = true, range = 1, minDamage = -3, maxDamage = -1, effect = 8
+	},
+}
+
+monster.defenses = {
+	defense = 1,
+	armor = 1,
+}
+
+monster.voices = {
+	interval = 5000,
+	chance = 10,
+	{ text = "Sssslll!", yell = false },
+	{ text = "liiimee!", yell = false },
+}
+
+mType:register(monster)
